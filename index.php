b91911a37cceda2c388b63c09584f61076170cb6
Kaarel Mäesalu <k44rel@protonmail.com>
2025-02-18 16:25:19 +0200

    Põhi alla

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Kodukas</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <h1>Kodukas</h1>
+    </body>
+</html>
